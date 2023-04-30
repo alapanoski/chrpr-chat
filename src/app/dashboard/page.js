f3c8ccf7784ba8249e61adcaa79ababf843d1f64
Allen Shibu <93600615+alapanoski@users.noreply.github.com>
2023-04-30 21:52:09 +0530

    Add auth

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,3 +1,9 @@
-export default function page() {
-  return <div className="text-red-500">fsddfdsafdsafdas</div>
+import { getServerSession } from "next-auth"
+
+import { authOptions } from "@/lib/auth"
+
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+
+  return <pre>{JSON.stringify(session)}</pre>
 }
